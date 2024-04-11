36db74411e40d80519e0a464bcff5e5ddb3252aa
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-04-11 13:04:28 -0400

    update dialogue

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,13 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `pc`,
-  'cs-degree': `cs-degree`,
-  'sofa-table': `sofa-table`,
-  tv: `tv`,
-  bed: `bed`,
-  resume: `resume`,
-  projects: `projects`,
-  library: `library`,
-  exit: `exit`
+  pc: `There's an open code editor and, oh look, Justin's GitHub profile.  I wonder what this guy has been up to...  <a href="https://github.com/themistogenes" target="_blank">View Justin's GitHub<a>`,
+  'cs-degree': `It appears to be a professional certificate from M.I.T. in Full Stack Web Development with MERN.  And I thought Justin only knows front-end stuff!`,
+  'sofa-table': `Hmmm...a sofa and table.  They look brand new.  I don't think Justin relaxes very often.`,
+  tv: `Isn't this the original Teenage Mutant Ninja Turtles movie?  Was this guy born in the '80s or something?`,
+  bed: `A cozy bed with noone in it.  Maybe I should take a nap?`,
+  resume: `It's Justin's resume.  Oh man was this hard to find!  <a href="https://themistogenes.github.io/assets/Justin-Alimaras-WebDev-Resume.pdf">Read Resume<a>`,
+  projects: `Looks like notes on some projects: <a href="https://house-marketplacev1.herokuapp.com" target="_blank">House Marketplace</a>, <a href="https://yelp-campgrounds-v2.herokuapp.com" target="_blank">Yelpcamp<a/>, and many others.  I bet Justin's portfolio is worth checking out.  <a href="https://themistogenes.github.io" target="_blank">View Portfolio</a>`,
+  library: `These books appear to be in Ancient Greek and Latin.  I thought this guy was a coder!  Maybe Justin was a Classicist in a previous life...`,
+  exit: `Looks like an exit back to Justin Alimaras' portfolio.  <a href="https://themistogenes.github.io">Exit</a>`
 }
