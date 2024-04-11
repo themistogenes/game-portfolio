cbf90caace87ea86efb0b31c50ddae8ac8cc060c
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-04-10 20:19:54 -0400

    create player

--- src/main.js
+++ src/main.js
@@ -22,7 +22,26 @@ k.scene('main', async () => {
   const mapData = await (await fetch('./map.json')).json();
   const layers = mapData.layers;
 
-  const map = k.make([k.sprite('map'), k.pos(0), k.scale(scaleFactor)]);
+  const map = k.make([
+    k.sprite('map'), 
+    k.pos(0), 
+    k.scale(scaleFactor)
+  ]);
+
+  const player = k.make([
+    k.sprite('spritesheet', {anim: 'idle-down'}), 
+    k.area({shape: new k.Rect(k.vec2(0, 3), 10, 10)}), 
+    k.body(),
+    k.anchor('center'),
+    k.pos(),
+    k.scale(scaleFactor),
+    {
+      speed: 250,
+      direction: 'down',
+      isInDialogue: false
+    },
+    'player'
+  ])
 })
 
 k.go('main');
