44dc3f8f7fff7d068230a795cf841ba9279099dc
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-04-10 20:10:24 -0400

    create map

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { scaleFactor } from './constants';
+import { k } from './kaboomCtx';
+
+k.loadSprite('spritesheet', './spritesheet.png', {
+  sliceX: 39,
+  sliceY: 31,
+  anims: {
+    'idle-down': 936,
+    'walk-down': { from: 936, to: 939, loop: true, speed: 8 },
+    'idle-side': 975,
+    'walk-side': { from: 975, to: 978, loop: true, speed: 8 },
+    'idle-up': 1014,
+    'walk-up': { from: 1014, to: 1017, loop: true, speed: 8 }
+  }
+});
+
+k.loadSprite('map', './map.png');
+
+k.setBackground(k.Color.fromHex('#311047'));
+
+k.scene('main', async () => {
+  const mapData = await (await fetch('./map.json')).json();
+  const layers = mapData.layers;
+
+  const map = k.make([k.sprite('map'), k.pos(0), k.scale(scaleFactor)]);
+})
+
+k.go('main');
