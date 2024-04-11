f7421c0ebea0567d993bf31d2ac71107e723f709
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-04-11 12:05:23 -0400

    camera scaling and player animations

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { scaleFactor } from './constants';
 import { k } from './kaboomCtx';
 import { displayDialogue } from './utils';
+import { setCamScale } from './utils';
 
 k.loadSprite('spritesheet', './spritesheet.png', {
   sliceX: 39,
@@ -80,6 +81,12 @@ k.scene('main', async () => {
     }
   }
 
+  setCamScale(k);
+
+  k.onResize(() => {
+    setCamScale(k);
+  })
+
   k.onUpdate(() => {
     k.camPos(player.pos.x, player.pos.y + 100);
   })
@@ -89,6 +96,63 @@ k.scene('main', async () => {
 
     const worldMousePos = k.toWorld(k.mousePos());
     player.moveTo(worldMousePos, player.speed);
+
+    const mouseAngle = player.pos.angle(worldMousePos);
+
+    const lowerBound = 50;
+    const upperBound = 125;
+
+    if (
+      mouseAngle > lowerBound &&
+      mouseAngle < upperBound &&
+      player.curAnim() !== 'walk-up'
+    ) {
+      player.play('walk-up');
+      player.direction = 'up';
+      return;
+    }
+
+    if (
+      mouseAngle < -lowerBound &&
+      mouseAngle > -upperBound &&
+      player.curAnim() !== 'walk-down'
+    ) {
+      player.play('walk-down');
+      player.direction = 'down';
+      return;
+    }
+
+    if (Math.abs(mouseAngle) > upperBound) {
+      player.flipX = false;
+      if (player.curAnim() !== 'walk-side') {
+        player.play('walk-side');
+        player.direction = 'right';
+        return;
+      }
+    }
+
+    if (Math.abs(mouseAngle) < lowerBound) {
+      player.flipX = true;
+      if (player.curAnim() !== 'walk-side') {
+        player.play('walk-side');
+        player.direction = 'left';
+        return;
+      }
+    }
+  })
+
+  k.onMouseRelease(() => {
+    if (player.direction === 'down') {
+      player.play('idle-down');
+      return;
+    }
+    
+    if (player.direction === 'up') {
+      player.play('idle-up');
+      return;
+    }
+
+    player.play('idle-side');
   })
 })
 
