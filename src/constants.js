0e35fa463a8dfba803ad26422733a6bc6a154098
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-03 22:53:52 -0400

    update link to resume

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const dialogueData = {
   'sofa-table': `Hmmm...a sofa and table.  They look brand new.  Justin should really take a load off!`,
   tv: `Isn't this the original Teenage Mutant Ninja Turtles movie?  Was this guy born in the '80s or something?`,
   bed: `This cozy bed looks perfect for a nap.  Hope Justin doesn't mind!`,
-  resume: `It's Justin's resume.  Oh man was this hard to find!  <a href="https://themistogenes.com/public/JustinAlimarasWebSoftwareDeveloper.pdf" target="_blank">Read Resume<a>`,
+  resume: `It's Justin's resume.  Oh man was this hard to find!  <a href="https://themistogenes.com/JustinAlimarasWebSoftwareDeveloper.pdf" target="_blank">Read Resume<a>`,
   projects: `Looks like notes on some projects: PropertyPulse, ProShop, HouseMarketplace, etc.  I'll have to check out the live versions on Justin's portfolio!`,
   library: `These books appear to be in Ancient Greek and Latin.  I thought this guy was a coder!  Maybe Justin was a Classicist in a previous life...`,
   exit: `Looks like an exit to Justin Alimaras' GitHub.  <a href="https://github.com/themistogenes" target="_blank">Exit</a>`
