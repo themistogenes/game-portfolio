dba43ebca38fd3ef745c47a9c950bd9977133de8
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-03 22:41:46 -0400

    update scaleFactor, dialogueData, and styling

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,14 @@
-export const scaleFactor = 4;
+// export const scaleFactor = 4;
+export const scaleFactor = 2;
 
 export const dialogueData = {
   pc: `There's an open code editor and, oh look, Justin's GitHub profile.  I wonder what this guy has been up to...  <a href="https://github.com/themistogenes" target="_blank">View Justin's GitHub<a>`,
-  'cs-degree': `It appears to be a professional certificate from M.I.T. in Full Stack Web Development with MERN.  And I thought Justin only knows front-end stuff!`,
-  'sofa-table': `Hmmm...a sofa and table.  They look brand new.  I don't think Justin relaxes very often.`,
+  'cs-degree': `It appears to be a professional certificate from M.I.T. in Full Stack Web Development with MERN.  And I thought Justin only knew front-end stuff...`,
+  'sofa-table': `Hmmm...a sofa and table.  They look brand new.  Justin should really take a load off!`,
   tv: `Isn't this the original Teenage Mutant Ninja Turtles movie?  Was this guy born in the '80s or something?`,
-  bed: `A cozy bed with noone in it.  Maybe I should take a nap?`,
-  resume: `It's Justin's resume.  Oh man was this hard to find!  <a href="https://themistogenes.github.io/assets/Justin-Alimaras-WebDev-Resume.pdf">Read Resume<a>`,
-  projects: `Looks like notes on some projects: <a href="https://house-marketplacev1.herokuapp.com" target="_blank">House Marketplace</a>, <a href="https://yelp-campgrounds-v2.herokuapp.com" target="_blank">Yelpcamp<a/>, and many others.  I bet Justin's portfolio is worth checking out.  <a href="https://themistogenes.github.io" target="_blank">View Portfolio</a>`,
+  bed: `This cozy bed looks perfect for a nap.  Hope Justin doesn't mind!`,
+  resume: `It's Justin's resume.  Oh man was this hard to find!  <a href="https://themistogenes.com/public/JustinAlimarasWebSoftwareDeveloper.pdf" target="_blank">Read Resume<a>`,
+  projects: `Looks like notes on some projects: PropertyPulse, ProShop, HouseMarketplace, etc.  I'll have to check out the live versions on Justin's portfolio!`,
   library: `These books appear to be in Ancient Greek and Latin.  I thought this guy was a coder!  Maybe Justin was a Classicist in a previous life...`,
-  exit: `Looks like an exit back to Justin Alimaras' portfolio.  <a href="https://themistogenes.github.io">Exit</a>`
+  exit: `Looks like an exit to Justin Alimaras' GitHub.  <a href="https://github.com/themistogenes" target="_blank">Exit</a>`
 }
